d52011648cc6664c4d55d567aea153b7b555722c
suryansh555-bit <suryanshsharman@gmail.com>
2026-03-02 08:25:55 +0530

    Add files via upload

---
+++ thirty three.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Node {
+    int coeff;
+    int exp;
+    struct Node* next;
+};
+
+
+struct Node* createNode(int c, int e) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->coeff = c;
+    newNode->exp = e;
+    newNode->next = NULL;
+    return newNode;
+}
+
+
+struct Node* insert(struct Node* head, int c, int e) {
+    struct Node* newNode = createNode(c, e);
+
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+void printPolynomial(struct Node* head) {
+    struct Node* temp = head;
+
+    while (temp != NULL) {
+        if (temp->exp > 1)
+            printf("%dx^%d", temp->coeff, temp->exp);
+        else if (temp->exp == 1)
+            printf("%dx", temp->coeff);
+        else
+            printf("%d", temp->coeff);
+
+        if (temp->next != NULL)
+            printf(" + ");
+
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node* head = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int coeff, exp;
+        scanf("%d %d", &coeff, &exp);
+        head = insert(head, coeff, exp);
+    }
+
+    printPolynomial(head);
+
+    return 0;
+}
